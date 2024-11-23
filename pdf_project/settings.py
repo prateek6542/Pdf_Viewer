790af95edaab1b07bf9821d709e4538c94175206
Prateek Sehgal <63642117+prateek6542@users.noreply.github.com>
2024-11-23 09:11:44 +0530

    Update settings.py

--- pdf_project/settings.py
+++ pdf_project/settings.py
@@ -116,6 +116,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 STATIC_URL = 'static/'
 
 # Default primary key field type
