3c1417276eeb828e48e968fc90492917c4279fc4
Prateek Sehgal <63642117+prateek6542@users.noreply.github.com>
2024-11-23 09:43:44 +0530

    Update settings.py

--- pdf_project/settings.py
+++ pdf_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-wu1d3ukc7@%5li5tn(0!^*($k4$u)81#28p3z(b!!0xovd(%v6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [pdf-viewer-k3kn.onrender.com]
+ALLOWED_HOSTS = ['pdf-viewer-k3kn.onrender.com']
 
 
 # Application definition
