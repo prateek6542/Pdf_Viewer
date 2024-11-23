0d887cc5287005b78bba0f2c5d0c670d6c6de7fd
Prateek Sehgal <63642117+prateek6542@users.noreply.github.com>
2024-11-23 10:48:57 +0530

    Update urls.py

--- pdf_project/urls.py
+++ pdf_project/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.shortcuts import redirect  # Import redirect function
+from django.conf import settings
+from django.conf.urls.static import static
 
 def redirect_to_upload(request):
     return redirect('pdf_upload')  # Ensure 'pdf_upload' is a valid name in app-level urls.py
@@ -10,3 +12,5 @@ urlpatterns = [
     path('upload/', include('pdf_manager.urls')),  # Connect app-level URLs
     path('', redirect_to_upload),  # Redirect root URL to /upload/
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
